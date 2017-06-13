51535c493858b2e806f4cf33e206b17cb0277117
dongmingjuan <d387918156@gmail.com>
2017-06-13 17:17:44 +0800

    finish the index and show by cycle

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,8 +1,12 @@
 class CasesController < ApplicationController
 
 	def index
+		@cases = Case.all
 	end
+
 	def show
+		@case = Case.find(params[:id])
+		@tasks = Task.where(case_id:@case)
 	end
 
 end
