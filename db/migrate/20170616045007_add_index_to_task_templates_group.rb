f5a8d82507d85313bdebf5228dff000b847fa359
Ji Xiang <jx4554@gmail.com>
2017-06-20 22:19:19 +0800

    update an old migration about index

--- db/migrate/20170616045007_add_index_to_task_templates_group.rb
+++ db/migrate/20170616045007_add_index_to_task_templates_group.rb
@@ -1,5 +1,5 @@
 class AddIndexToTaskTemplatesGroup < ActiveRecord::Migration[5.0]
   def change
-  	add_index :task_templates_groups, :task_template_id
+  	add_index :task_templates, :task_templates_group_id
   end
 end
