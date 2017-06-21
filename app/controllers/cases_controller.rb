32e20cfca4794ecdefd1a659337dfee9705ac49c
Ji Xiang <jx4554@gmail.com>
2017-06-21 18:20:55 +0800

    update humanize fucntion

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -153,14 +153,15 @@ class CasesController < ApplicationController
 	private
 
 	def humanize(secs)
-		[[60, "秒"], [60, "分"], [24, "小时"], [1000, "天"]].map{|count, name|
-			if secs > 0
-				secs, n = secs.divmod(count)
-				"#{n.to_i} #{name}"
-			end
-		}.compact.reverse.join(' ')
 		if secs == 0
 			"0秒"
+		else
+			[[60, "秒"], [60, "分"], [24, "小时"], [1000, "天"]].map{|count, name|
+				if secs > 0
+					secs, n = secs.divmod(count)
+					"#{n.to_i} #{name}"
+				end
+			}.compact.reverse.join(' ')
 		end
 	end
 
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,14 +2,15 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   	def humanize(secs)
-		[[60, "秒"], [60, "分"], [24, "小时"], [1000, "天"]].map{|count, name|
-			if secs > 0
-				secs, n = secs.divmod(count)
-				"#{n.to_i} #{name}"
-			end
-		}.compact.reverse.join(' ')
-		if secs == 0
+  		if secs == 0
 			"0秒"
+		else
+			[[60, "秒"], [60, "分"], [24, "小时"], [1000, "天"]].map{|count, name|
+				if secs > 0
+					secs, n = secs.divmod(count)
+					"#{n.to_i} #{name}"
+				end
+			}.compact.reverse.join(' ')
 		end
 	end
 end
