2ee8cf477c3757a042c177e1dacbcadb37b256f7
Ji Xiang <jx4554@gmail.com>
2017-06-28 10:40:43 +0800

    article title bug

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   def index
-  	if category = params[:category]
-  		@articles = Article.where(category: category).order("created_at DESC")
+  	if @category = params[:category]
+  		@articles = Article.where(category: @category).order("created_at DESC")
   	else
   		@articles = Article.all.order("created_at DESC")
   	end
